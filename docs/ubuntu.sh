82d3f11f914bea3d1379cf7bb307bedc773b05ee
Axodouble <git@axodouble.com>
2025-08-09 22:20:09 +0800

    Updated ubuntu install script

--- docs/ubuntu.sh
+++ docs/ubuntu.sh
@@ -18,16 +18,59 @@ sudo apt install -y \
     git \
     gh \
     gcc \
-    unzip
+    unzip \
+    make \
+    build-essential
+
+# Install python build essentials
+sudo apt install -y libssl-dev zlib1g-dev \
+    libbz2-dev libreadline-dev libsqlite3-dev \
+    libncursesw5-dev xz-utils tk-dev libxml2-dev \
+    libxmlsec1-dev libffi-dev liblzma-dev
+
+# Install Bun
+curl -fsSL https://bun.sh/install | bash
+
+# Install Pyenv if not already installed
+if ! command -v pyenv &> /dev/null; then
+    curl https://pyenv.run | bash
+fi
+
+PYENV_ROOT='export PYENV_ROOT="$HOME/.pyenv"'
+PYENV_PATH='[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"'
+PYENV_INIT='eval "$(pyenv init - bash)"'
+
+if ! grep -Fxq "$PYENV_ROOT" ~/.bashrc; then
+    echo "$PYENV_ROOT" >> ~/.bashrc
+fi
+if ! grep -Fxq "$PYENV_PATH" ~/.bashrc; then
+    echo "$PYENV_PATH" >> ~/.bashrc
+fi
+if ! grep -Fxq "$PYENV_INIT" ~/.bashrc; then
+    echo "$PYENV_INIT" >> ~/.bashrc
+fi
+if ! grep -Fxq "$PYENV_ROOT" ~/.profile; then
+    echo "$PYENV_ROOT" >> ~/.profile
+fi
+if ! grep -Fxq "$PYENV_PATH" ~/.profile; then
+    echo "$PYENV_PATH" >> ~/.profile
+fi
+if ! grep -Fxq "$PYENV_INIT" ~/.profile; then
+    echo "$PYENV_INIT" >> ~/.profile
+fi
 
 # Install yt-dlp
 sudo apt install -y yt-dlp
 
-# Install Tailscale
-curl -fsSL https://tailscale.com/install.sh | sh
+# Install Tailscale if not already installed
+if ! command -v tailscale &> /dev/null; then
+    curl -fsSL https://tailscale.com/install.sh | sh
+fi
 
-# Install Brew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# Install Brew if not already installed
+if ! command -v brew &> /dev/null; then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
 
 # Add yt-dlp alias to .bashrc if not already present
 ALIAS_CMD="alias d=\"yt-dlp -o '~/Memes/%(title)s.%(ext)s'\""
@@ -35,11 +78,10 @@ if ! grep -Fxq "$ALIAS_CMD" ~/.bashrc; then
     echo "$ALIAS_CMD" >> ~/.bashrc
 fi
 
-# Install Bun
-curl -fsSL https://bun.sh/install | bash
-
-# Install Docker
-curl -fsSL https://get.docker.com | sudo sh
-
-# Add user to docker group
-sudo usermod -aG docker $USER
+# Install Docker if not already installed
+if ! command -v docker &> /dev/null; then
+    curl -fsSL https://get.docker.com | sudo sh
+    
+    # Add user to docker group
+    sudo usermod -aG docker $USER
+fi
