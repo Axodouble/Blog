7a4214481043277533f8f5e009d6fc2ac1db3955
Axodouble <git@axodouble.com>
2025-10-01 21:07:49 +0800

    Updated ubuntu command to also install neovim + plugins

--- docs/ubuntu.sh
+++ docs/ubuntu.sh
@@ -11,31 +11,31 @@ sudo add-apt-repository ppa:tomtomtom/yt-dlp
 sudo apt update
 sudo apt upgrade -y
 sudo apt install -y \
-    distrobox \
-    neofetch \
-    curl \
-    net-tools \
-    htop \
-    btop \
-    git \
-    gh \
-    gcc \
-    unzip \
-    make \
-    build-essential \
-    golang \
-    flatpak \
-    gnome-software-plugin-flatpak
+  distrobox \
+  neofetch \
+  curl \
+  net-tools \
+  htop \
+  btop \
+  git \
+  gh \
+  gcc \
+  unzip \
+  make \
+  build-essential \
+  golang \
+  flatpak \
+  gnome-software-plugin-flatpak
 
 # Install python build essentials
 sudo apt install -y libssl-dev zlib1g-dev \
-    libbz2-dev libreadline-dev libsqlite3-dev \
-    libncursesw5-dev xz-utils tk-dev libxml2-dev \
-    libxmlsec1-dev libffi-dev liblzma-dev
+  libbz2-dev libreadline-dev libsqlite3-dev \
+  libncursesw5-dev xz-utils tk-dev libxml2-dev \
+  libxmlsec1-dev libffi-dev liblzma-dev
 
 # Install Bun
-if ! command -v bun &> /dev/null; then
-    curl -fsSL https://bun.sh/install | bash
+if ! command -v bun &>/dev/null; then
+  curl -fsSL https://bun.sh/install | bash
 fi
 
 # Install yt-dlp
@@ -44,17 +44,17 @@ sudo apt install -y yt-dlp
 # Add yt-dlp alias to .bashrc if not already present
 ALIAS_CMD="alias d=\"yt-dlp -o '~/Memes/%(title)s.%(ext)s'\""
 if ! grep -Fxq "$ALIAS_CMD" ~/.bashrc; then
-    echo "$ALIAS_CMD" >> ~/.bashrc
+  echo "$ALIAS_CMD" >>~/.bashrc
 fi
 
 # Install Node Version Manager
-if ! command -v nvm &> /dev/null; then
-    curl -fsSL https://raw.githubusercontent.com/nvm-sh/nvm/refs/heads/master/install.sh | bash
+if ! command -v nvm &>/dev/null; then
+  curl -fsSL https://raw.githubusercontent.com/nvm-sh/nvm/refs/heads/master/install.sh | bash
 fi
 
 # Install Pyenv if not already installed
-if ! command -v pyenv &> /dev/null; then
-    curl https://pyenv.run | bash
+if ! command -v pyenv &>/dev/null; then
+  curl https://pyenv.run | bash
 fi
 
 PYENV_ROOT='export PYENV_ROOT="$HOME/.pyenv"'
@@ -62,56 +62,55 @@ PYENV_PATH='[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"'
 PYENV_INIT='eval "$(pyenv init - bash)"'
 
 if ! grep -Fxq "$PYENV_ROOT" ~/.bashrc; then
-    echo "$PYENV_ROOT" >> ~/.bashrc
+  echo "$PYENV_ROOT" >>~/.bashrc
 fi
 if ! grep -Fxq "$PYENV_PATH" ~/.bashrc; then
-    echo "$PYENV_PATH" >> ~/.bashrc
+  echo "$PYENV_PATH" >>~/.bashrc
 fi
 if ! grep -Fxq "$PYENV_INIT" ~/.bashrc; then
-    echo "$PYENV_INIT" >> ~/.bashrc
+  echo "$PYENV_INIT" >>~/.bashrc
 fi
 if ! grep -Fxq "$PYENV_ROOT" ~/.profile; then
-    echo "$PYENV_ROOT" >> ~/.profile
+  echo "$PYENV_ROOT" >>~/.profile
 fi
 if ! grep -Fxq "$PYENV_PATH" ~/.profile; then
-    echo "$PYENV_PATH" >> ~/.profile
+  echo "$PYENV_PATH" >>~/.profile
 fi
 if ! grep -Fxq "$PYENV_INIT" ~/.profile; then
-    echo "$PYENV_INIT" >> ~/.profile
+  echo "$PYENV_INIT" >>~/.profile
 fi
 
-
 # Install Tailscale if not already installed
-if ! command -v tailscale &> /dev/null; then
-    curl -fsSL https://tailscale.com/install.sh | sh
+if ! command -v tailscale &>/dev/null; then
+  curl -fsSL https://tailscale.com/install.sh | sh
 fi
 
 # Install Brew if not already installed
-if ! command -v brew &> /dev/null; then
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+if ! command -v brew &>/dev/null; then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-    BREW='eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"'
+  BREW='eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"'
 
-    if ! grep -Fxq "$BREW" ~/.bashrc; then
-        echo "$BREW" >> ~/.bashrc
-    fi
-    if ! grep -Fxq "$BREW" ~/.profile; then
-        echo "$BREW" >> ~/.profile
-    fi
+  if ! grep -Fxq "$BREW" ~/.bashrc; then
+    echo "$BREW" >>~/.bashrc
+  fi
+  if ! grep -Fxq "$BREW" ~/.profile; then
+    echo "$BREW" >>~/.profile
+  fi
 
-    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+  eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 fi
 
 # Install Docker if not already installed
-if ! command -v docker &> /dev/null && ! which docker &> /dev/null && ! [ -x "/usr/bin/docker" ]; then
-    echo "Docker not found, installing..."
-    curl -fsSL https://get.docker.com | sudo sh
-    
-    # Add user to docker group
-    sudo usermod -aG docker $USER
-    echo "Docker installation completed."
+if ! command -v docker &>/dev/null && ! which docker &>/dev/null && ! [ -x "/usr/bin/docker" ]; then
+  echo "Docker not found, installing..."
+  curl -fsSL https://get.docker.com | sudo sh
+
+  # Add user to docker group
+  sudo usermod -aG docker $USER
+  echo "Docker installation completed."
 else
-    echo "Docker is already installed, skipping installation."
+  echo "Docker is already installed, skipping installation."
 fi
 
 # Install gobonsai
@@ -121,3 +120,7 @@ curl -fsSL https://raw.githubusercontent.com/Axodouble/gobonsai/refs/heads/maste
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
 brew install lazydocker lazygit
+
+git clone https://github.com/LazyVim/starter ~/.config/nvim
+rm -rf ~/.config/nvim/.git
+sudo snap install nvim --classic
