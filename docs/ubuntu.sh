4867c3bb55ab5ac448a86e0061b60845f67b1b74
Axodouble <git@axodouble.com>
2025-10-01 21:59:08 +0800

    Fixed confirmation and added fzf to installation

--- docs/ubuntu.sh
+++ docs/ubuntu.sh
@@ -123,5 +123,5 @@ brew install lazydocker lazygit
 
 git clone https://github.com/LazyVim/starter ~/.config/nvim
 rm -rf ~/.config/nvim/.git
-sudo apt install ripgrep fd-find
+sudo apt install ripgrep fd-find fzf -y
 sudo snap install nvim --classic
